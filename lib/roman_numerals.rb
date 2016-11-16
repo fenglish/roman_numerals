5254f7bd6dc00c05185ffc54744dac7781cfb3aa
Asuka Ochi <asukaochi@gmail.com>
2016-11-16 09:32:49 +0000

    the one's place done

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,10 +1,19 @@
 class RomanNumerals
 
   def convert(number)
-    roman_numerals = { 1 => :I, 5 => :V, 
-                       10 => :X, 50 => :L,
-                       100 => :C, 500 => :D, 1000 => :M}
-    return roman_numerals[number].to_s
+    answer = []
+    roman_numerals = { 1 => :I, 4 => :IV, 5 => :V,
+                       10 => :X, 40 => :XL, 50 => :L,
+                       100 => :C, 400 => :CD, 500 => :D, 1000 => :M}
+    return roman_numerals[number].to_s if !roman_numerals[number].nil?
+    jyu = number / 10
+    ichi = number - jyu * 10
+    if ichi > 5
+      ichi -= 5
+      answer << roman_numerals[5].to_s
+    end
+    ichi.times{ answer << roman_numerals[1].to_s }
+    answer.inject(:+)
   end
 
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -3,32 +3,53 @@ require "roman_numerals.rb"
 describe RomanNumerals do
   subject(:roman_numeral){ described_class.new }
 
-  it "should return 'I' when passed 1" do
-    expect(roman_numeral.convert(1)).to eq "I"
-  end
+    it "should return 'I' when passed 1" do
+      expect(roman_numeral.convert(1)).to eq "I"
+    end
 
-  it "should return 'V' when passed 5" do
-    expect(roman_numeral.convert(5)).to eq "V"
-  end
+    it "should return 'IV' when passed 4" do
+      expect(roman_numeral.convert(4)).to eq "IV"
+    end
 
-  it "should return 'X' when passed 10" do
-    expect(roman_numeral.convert(10)).to eq "X"
-  end
+    it "should return 'V' when passed 5" do
+      expect(roman_numeral.convert(5)).to eq "V"
+    end
 
-  it "should return 'L' when passed 50" do
-    expect(roman_numeral.convert(50)).to eq "L"
-  end
+    it "should return 'X' when passed 10" do
+      expect(roman_numeral.convert(10)).to eq "X"
+    end
 
-  it "should return 'C' when passed 100" do
-    expect(roman_numeral.convert(100)).to eq "C"
-  end
+    it "should return 'X' when passed 40" do
+      expect(roman_numeral.convert(40)).to eq "XL"
+    end
 
-  it "should return 'D' when passed 500" do
-    expect(roman_numeral.convert(500)).to eq "D"
-  end
+    it "should return 'L' when passed 50" do
+      expect(roman_numeral.convert(50)).to eq "L"
+    end
 
-  it "should return 'M' when passed 1000" do
-    expect(roman_numeral.convert(1000)).to eq "M"
-  end
+    it "should return 'C' when passed 100" do
+      expect(roman_numeral.convert(100)).to eq "C"
+    end
+
+    it "should return 'CD' when passed 400" do
+      expect(roman_numeral.convert(400)).to eq "CD"
+    end
+
+    it "should return 'D' when passed 500" do
+      expect(roman_numeral.convert(500)).to eq "D"
+    end
+
+    it "should return 'M' when passed 1000" do
+      expect(roman_numeral.convert(1000)).to eq "M"
+    end
+
+    it "should return 'II' when passed 2" do
+      expect(roman_numeral.convert(2)).to eq "II"
+    end
+
+    it "should return 'VI' when passed 6" do
+      expect(roman_numeral.convert(6)).to eq "VI"
+    end
+
 
 end
