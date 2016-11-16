6f9cb8d7326361cb6b738e5582be5727c4441b44
Asuka Ochi <asukaochi@gmail.com>
2016-11-16 09:01:19 +0000

    refactering

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,13 +1,10 @@
 class RomanNumerals
 
   def convert(number)
-    return "I" if number == 1
-    return "V" if number == 5
-    return "X" if number == 10
-    return "L" if number == 50
-    return "C" if number == 100
-    return "D" if number == 500
-    return "M" if number == 1000
+    roman_numerals = { 1 => :I, 5 => :V, 
+                       10 => :X, 50 => :L,
+                       100 => :C, 500 => :D, 1000 => :M}
+    return roman_numerals[number].to_s
   end
 
 end
